a9bae329cfe3cf405f170ca6253db3b6e1aa653b
Zeyad Lotfy <zezoramy380@gmail.com>
2025-02-20 21:22:17 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setViewEngine('ejs');
+  app.useGlobalPipes(new ValidationPipe());
+
+  const swagger = new DocumentBuilder()
+    .setTitle('Collabry API')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT token',
+      in: 'header',
+    })
+    .build();
+  const documentation = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup('swagger', app, documentation);
+
   await app.listen(4000);
 }
 bootstrap();
