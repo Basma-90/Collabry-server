4b1d9398c6292ab45c8997baaf20a45c3e368f9e
Zeyad Lotfy <zezoramy380@gmail.com>
2025-02-21 15:55:59 +0200

    cloudinary config

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { CloudinaryProvider } from 'src/storage/cloudinary/cloudinary.provider';
+import { CloudinaryService } from 'src/storage/cloudinary/cloudinary.service';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  controllers: [UsersController],
+  providers: [
+    UsersService,
+    PrismaService,
+    CloudinaryProvider,
+    CloudinaryService,
+  ],
+})
 export class UsersModule {}
