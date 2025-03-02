da3e9e27f1b1256df644bb19ecf70b346250a109
Zeyad Lotfy <zezoramy380@gmail.com>
2025-03-02 02:41:43 +0200

    enhance publication module

--- src/modules/publications/publications.service.ts
+++ src/modules/publications/publications.service.ts
@@ -52,6 +52,132 @@ export class PublicationsService {
     return 'Publication created successfully';
   }
 
+  async getSinglePublication(userId: string, publicationId: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        publications: true,
+      },
+    });
+    if (
+      !user ||
+      !user.publications.find((publication) => publication.id === publicationId)
+    ) {
+      throw new UnauthorizedException();
+    }
+    const publication = await this.prisma.publication.findUnique({
+      where: {
+        id: publicationId,
+      },
+      include: {
+        sections: {
+          include: {
+            files: true,
+          },
+        },
+        category: true,
+      },
+    });
+    return {
+      id: publication.id,
+      title: publication.title,
+      abstract: publication.abstract,
+      keywords: publication.keywords,
+      language: publication.language,
+      visibility: publication.visibility,
+      status: publication.status,
+      sections: publication.sections.map((section) => ({
+        id: section.id,
+        title: section.title,
+        orderIndex: section.orderIndex,
+        type: section.type,
+        content: section.content,
+        files: section.files.map((file) => ({
+          id: file.id,
+          url: file.url,
+        })),
+      })),
+      categoryName: publication.category.name,
+      categoryId: publication.category.id,
+      createdAt: publication.createdAt,
+    };
+  }
+
+  async getAllPublicationsForUser(userId: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        publications: true,
+      },
+    });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const publications = await this.prisma.publication.findMany({
+      where: {
+        authorId: userId,
+      },
+      include: {
+        category: true,
+      },
+    });
+    return publications.map((publication) => ({
+      id: publication.id,
+      title: publication.title,
+      abstract: publication.abstract,
+      keywords: publication.keywords,
+      language: publication.language,
+      visibility: publication.visibility,
+      status: publication.status,
+      categoryName: publication.category.name,
+      categoryId: publication.category.id,
+      createdAt: publication.createdAt,
+    }));
+  }
+
+  async changePublication(
+    userId: string,
+    publicationId: string,
+    createPublicationDto: CreatePublicationDto,
+  ) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const publication = await this.prisma.publication.findUnique({
+      where: {
+        id: publicationId,
+      },
+      select: {
+        authorId: true,
+      },
+    });
+    if (!publication || publication.authorId !== userId) {
+      throw new UnauthorizedException();
+    }
+    await this.prisma.publication.update({
+      where: {
+        id: publicationId,
+      },
+      data: {
+        title: createPublicationDto.title,
+        abstract: createPublicationDto.abstract,
+        keywords: createPublicationDto.keywords,
+        language: createPublicationDto.language,
+        visibility: createPublicationDto.visibility,
+      },
+    });
+    return 'Publication updated successfully';
+  }
+
   async createSection(
     userId: string,
     sectionDto: SectionDto,
@@ -133,63 +259,6 @@ export class PublicationsService {
     return 'section cerated successfully';
   }
 
-  async getPublication(userId: string, publicationId: string) {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-      select: {
-        publications: true,
-      },
-    });
-
-    if (
-      !user ||
-      !user.publications.find((publication) => publication.id === publicationId)
-    ) {
-      throw new UnauthorizedException();
-    }
-    const publication = await this.prisma.publication.findUnique({
-      where: {
-        id: publicationId,
-      },
-      include: {
-        sections: {
-          include: {
-            files: true,
-          },
-        },
-        category: true,
-      },
-    });
-
-    return {
-      id: publication.id,
-      title: publication.title,
-      abstract: publication.abstract,
-      keywords: publication.keywords,
-      language: publication.language,
-      visibility: publication.visibility,
-      status: publication.status,
-      views: publication.views,
-      citations: publication.citations,
-      categoryName: publication.category.name,
-      categoryId: publication.category.id,
-      sections: publication.sections.map((section) => ({
-        id: section.id,
-        title: section.title,
-        orderIndex: section.orderIndex,
-        type: section.type,
-        content: section.content,
-        files: section.files.map((file) => ({
-          id: file.id,
-          url: file.url,
-        })),
-      })),
-      createdAt: publication.createdAt,
-    };
-  }
-
   async changePublicationStatus(
     userId: string,
     publicationId: string,
@@ -251,5 +320,283 @@ export class PublicationsService {
       },
     });
     return 'Publication visibility changed successfully';
+  }
+
+  async getAllPublications() {
+    const publications = await this.prisma.publication.findMany({
+      where: {
+        visibility: 'PUBLIC',
+        status: 'PUBLISHED',
+      },
+      include: {
+        category: true,
+        author: true,
+        sections: {
+          include: {
+            files: true,
+          },
+        },
+      },
+    });
+
+    return publications.map((publication) => ({
+      id: publication.id,
+      title: publication.title,
+      abstract: publication.abstract,
+      keywords: publication.keywords,
+      language: publication.language,
+      visibility: publication.visibility,
+      sections: publication.sections.map((section) => ({
+        id: section.id,
+        title: section.title,
+        orderIndex: section.orderIndex,
+        type: section.type,
+        content: section.content,
+        files: section.files.map((file) => ({
+          id: file.id,
+          url: file.url,
+        })),
+      })),
+      categoryName: publication.category.name,
+      categoryId: publication.category.id,
+      authorName: publication.author.email,
+      authorId: publication.author.id,
+      createdAt: publication.createdAt,
+    }));
+  }
+
+  async getPublication(publicationId: string) {
+    const publication = await this.prisma.publication.findUnique({
+      where: {
+        id: publicationId,
+        visibility: 'PUBLIC',
+        status: 'PUBLISHED',
+      },
+      include: {
+        category: true,
+        author: true,
+        sections: {
+          include: {
+            files: true,
+          },
+        },
+      },
+    });
+
+    if (!publication) {
+      throw new UnauthorizedException();
+    }
+    return {
+      id: publication.id,
+      title: publication.title,
+      abstract: publication.abstract,
+      keywords: publication.keywords,
+      language: publication.language,
+      visibility: publication.visibility,
+      sections: publication.sections.map((section) => ({
+        id: section.id,
+        title: section.title,
+        orderIndex: section.orderIndex,
+        type: section.type,
+        content: section.content,
+        files: section.files.map((file) => ({
+          id: file.id,
+          url: file.url,
+        })),
+      })),
+      categoryName: publication.category.name,
+      categoryId: publication.category.id,
+      authorName: publication.author.email,
+      authorId: publication.author.id,
+      createdAt: publication.createdAt,
+    };
+  }
+
+  // ------------------------------------------------
+  async getSingleSection(userId: string, sectionId: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        publications: true,
+      },
+    });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const section = await this.prisma.section.findUnique({
+      where: {
+        id: sectionId,
+      },
+      include: {
+        files: true,
+        publication: {
+          select: {
+            id: true,
+            title: true,
+          },
+        },
+      },
+    });
+    if (
+      !user.publications.find(
+        (publication) => publication.id === section.publicationId,
+      )
+    ) {
+      throw new UnauthorizedException();
+    }
+    return {
+      id: section.id,
+      title: section.title,
+      orderIndex: section.orderIndex,
+      type: section.type,
+      content: section.content,
+      publicationId: section.publicationId,
+      publicationTitle: section.publication.title,
+      files: section.files.map((file) => ({
+        id: file.id,
+        url: file.url,
+      })),
+    };
+  }
+
+  async updateSection(
+    userId: string,
+    sectionId: string,
+    sectionDto: SectionDto,
+  ) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        publications: true,
+      },
+    });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const section = await this.prisma.section.findUnique({
+      where: {
+        id: sectionId,
+      },
+      include: {
+        publication: {
+          select: {
+            authorId: true,
+          },
+        },
+      },
+    });
+    if (!section || section.publication.authorId !== userId) {
+      throw new UnauthorizedException();
+    }
+    await this.prisma.section.update({
+      where: {
+        id: sectionId,
+      },
+      data: {
+        title: sectionDto.title,
+        orderIndex: parseInt(sectionDto.orderIndex),
+        type: sectionDto.type,
+        content: sectionDto.content,
+      },
+    });
+    return 'Section updated successfully';
+  }
+
+  async addSectionFile(
+    userId: string,
+    sectionId: string,
+    file: Express.Multer.File,
+  ) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        publications: true,
+      },
+    });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const section = await this.prisma.section.findUnique({
+      where: {
+        id: sectionId,
+      },
+      include: {
+        publication: {
+          select: {
+            authorId: true,
+          },
+        },
+      },
+    });
+    if (!section || section.publication.authorId !== userId) {
+      throw new UnauthorizedException();
+    }
+    if (file) {
+      const buffer = await this.cloudinaryService.uploadFile(file);
+      if (buffer) {
+        await this.prisma.sectionFile.create({
+          data: {
+            url: buffer.url,
+            publicId: buffer.public_id,
+            section: {
+              connect: {
+                id: sectionId,
+              },
+            },
+          },
+        });
+        return 'File added successfully';
+      } else {
+        throw new ConflictException('Failed to upload file');
+      }
+    } else {
+      throw new ConflictException('File not found');
+    }
+  }
+
+  async deleteSectionFile(userId: string, fileId: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        publications: true,
+      },
+    });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const file = await this.prisma.sectionFile.findUnique({
+      where: {
+        id: fileId,
+      },
+      include: {
+        section: {
+          include: {
+            publication: {
+              select: {
+                authorId: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    if (!file || file.section.publication.authorId !== userId) {
+      throw new UnauthorizedException();
+    }
+    await this.cloudinaryService.deleteFile(file.publicId);
+    await this.prisma.sectionFile.delete({
+      where: {
+        id: fileId,
+      },
+    });
+    return 'File deleted successfully';
   }
 }
